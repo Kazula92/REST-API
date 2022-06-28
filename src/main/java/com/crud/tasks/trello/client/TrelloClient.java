2578de1a7087291f7f85a41a6181f105ba5fbda8
Krzysztof.Kazula <kkazula92@gmail.com>
2022-06-28 23:41:54 +0200

    22.3 Customer expansion

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,6 +1,8 @@
 package com.crud.tasks.trello.client;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -31,7 +33,9 @@ public class TrelloClient {
         URI url= UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUser + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id").build().encode().toUri();
+                .queryParam("fields", "name,id")
+                .queryParam("lists", "all")
+                .build().encode().toUri();
 
 
         TrelloBoardDto[] boardsResponse= restTemplate.getForObject(url, TrelloBoardDto[].class);
@@ -42,5 +46,18 @@ public class TrelloClient {
                 .filter(p -> Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
                 .filter(p -> p.getName().contains("Kodilla"))
                 .collect(Collectors.toList());
+    }
+
+    public CreatedTrelloCard createNewCard (TrelloCardDto trelloCardDto) {
+        URI url= UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/cards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("name", trelloCardDto.getName())
+                .queryParam("desc", trelloCardDto.getDescription())
+                .queryParam("pos", trelloCardDto.getPos())
+                .queryParam("idList", trelloCardDto.getListId())
+                .build().encode().toUri();
+
+        return restTemplate.postForObject(url, null, CreatedTrelloCard.class);
     }
 }
