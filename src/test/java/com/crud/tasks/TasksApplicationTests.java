a0c2bcbaab29ed6e156a23655087fe5fbb72b31f
Krzysztof.Kazula <kkazula92@gmail.com>
2022-07-24 22:33:20 +0200

    Initial commit

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,9 +1,9 @@
 package com.crud.tasks;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TasksApplicationTests {
 
 	@Test
