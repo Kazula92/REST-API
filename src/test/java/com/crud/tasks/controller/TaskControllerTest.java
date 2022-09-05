30ffa3f50f15d52104148f6d73e9ad88b088f099
Krzysztof.Kazula <kkazula92@gmail.com>
2022-09-05 20:55:16 +0200

    29.3 Test_TaskController

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -4,7 +4,6 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
-import com.crud.tasks.trello.facade.TrelloFacade;
 import com.google.gson.Gson;
 import lombok.SneakyThrows;
 import org.hamcrest.Matchers;
@@ -21,9 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringJUnitWebConfig
 @WebMvcTest(TaskController.class)
@@ -55,7 +52,7 @@ class TaskControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].title", Matchers.is("title")))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].content", Matchers.is("content")));
     }
-
+    @SneakyThrows
     @Test
     void shouldGetTask() throws Exception {
         //Given
@@ -97,26 +94,4 @@ class TaskControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.is("content")));
     }
 
-    @Test
-    void shouldCreateTask() throws Exception {
-        //Given
-        Task task = new Task(1L, "title", "content");
-        TaskDto taskDto = new TaskDto(1L, "title", "content");
-        when(taskMapper.mapToTask(any(TaskDto.class))).thenReturn(task);
-        when(dbService.saveTask(task)).thenReturn(task);
-        when(taskMapper.mapToTaskDto(any(Task.class))).thenReturn(taskDto);
-        Gson gson = new Gson();
-        String jsonContent = gson.toJson(taskDto);
-
-        // When & Then
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .post("/v1/tasks/createTask")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("UTF-8")
-                        .content(jsonContent))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is("title")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.is("content")));
-    }
 }
